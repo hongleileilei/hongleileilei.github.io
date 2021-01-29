c1d827be6fdbbead81fab97954f2d8882abd7d88
hongleileilei <39015103+hongleileilei@users.noreply.github.com>
2021-01-29 08:54:43 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+var main = function() {
+    // navigate nav
+    $("#nav a span").click(function() {
+      var firstChar = $(this)
+        .text()
+        .charAt(0);
+      var newPos = 0;
+      switch (firstChar) {
+        case "A":
+          newPos = $("#about").offset().top;
+          break;
+        case "R":
+          newPos = $("#researches").offset().top;
+          break;
+        case "P":
+            newPos = $("#projects").offset().top;
+            break;
+        case "C":
+          newPos = $("#contact").offset().top;
+          break;
+      }
+  
+      newPos -= $("header").outerHeight();
+      $(document).scrollTop(newPos);
+    });
+  };
+  
+  $(document).ready(main);
+  
